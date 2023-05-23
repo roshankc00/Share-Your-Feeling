17b04200559d27a638912739cac6c39ee2794cdc
Roshan karki <rohitkc8848@gmail.com>
2023-05-24 00:35:31 +0545

    feat:folder setup

--- index.js
+++ index.js
@@ -1,11 +1,25 @@
 const express=require("express")
+const cors=require('cors')
+// connecting to the database 
 
+// rest variables
 const app=express()
 const port=5000
 
+
+// routes imports
+
+
+// middlewares
+app.use(express.json())
+app.use(cors())
+
+
+// all the routes 
 app.get("/",(req,res)=>{
     res.send("from server")
 })
+// listening to the port 
 app.listen(port,()=>{
     console.log(`listening at the port ${port}`)
 })
